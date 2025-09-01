bbd3c6fcfd9967723992b99d1dc589d89ba351ea
Tadano GT-750EL <anthony.gabriel29082009@gmail.com>
2025-09-01 11:37:27 -0300

    Add files via upload

---
+++ automação.py
@@ -0,0 +1,42 @@
+import pyautogui, mouseinfo
+
+# pyautogui.moveTo(cordenada1,cordenada2, duration=?)
+# pyautogui.click() clica clica clica
+# pyautogui.write("Digitano")
+# pyautogui.doubleclick() dois
+# pyautogui.drag() LEAVAOOCVASDSA-
+mouseinfo.mouseInfo()
+
+# jogar joguin de musga ali ó..
+# Usa quando o python tiver com 3 aba aberta
+# e se precisar.. reposicione a primeira parte do programa para abrir o operaGx
+
+pyautogui.moveTo(1098,1061, duration=0.3)
+pyautogui.click()
+pyautogui.moveTo(665,22, duration=0.3)
+pyautogui.click()
+pyautogui.moveTo(503,58, duration=0.3)
+pyautogui.click()
+pyautogui.write("Rhythm Game")
+pyautogui.press('enter')
+pyautogui.sleep(4)
+pyautogui.moveTo(714,373, duration=0.5)
+pyautogui.click()
+pyautogui.sleep(8.5)
+pyautogui.moveTo(1034,638, duration=1.55)
+pyautogui.click()
+pyautogui.sleep(4)
+pyautogui.moveTo(1160,776, duration=0.5)
+pyautogui.click()
+pyautogui.sleep(2)
+pyautogui.moveTo(1412,734, duration=0.8)
+pyautogui.click()
+pyautogui.sleep(9.5)
+pyautogui.moveTo(1103,484, duration=0.3)
+pyautogui.moveTo(442,829, duration=8)
+pyautogui.sleep(2)
+pyautogui.moveTo(1896,24, duration=5)
+pyautogui.sleep(6)
+pyautogui.moveTo(1001,527, duration=0.15)
+pyautogui.click()
+pyautogui.moveTo(1919,1079, duration=0.005)
